2ced45eb27ed4e8f8b992dc1a27f8d807b9760c9
joypatel04 <joypatel041994@gmail.com>
2017-03-31 14:26:08 -0700

    Added App file to run main screen

---
+++ src/App.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Open up main.js to start working on your app!</Text>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
