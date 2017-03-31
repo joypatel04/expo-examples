ba88eec9bdd11226253fd40d82418bc5b73e7f67
joypatel04 <joypatel041994@gmail.com>
2017-03-31 14:25:47 -0700

    Added Redux approch

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 import Expo from 'expo';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import store from './src/store/configureStore' 
 import { Provider } from 'react-redux';
+import App from './src/App';
 
 class Root extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <App />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 Expo.registerRootComponent(Root);
