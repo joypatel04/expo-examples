2922cf7aff920f7720fdf3d6d7b66af33908ea56
joypatel04 <joypatel041994@gmail.com>
2017-04-16 19:29:53 -0700

    Fix Spaces to 2

--- src/services/display.js
+++ src/services/display.js
@@ -1,27 +1,32 @@
-var text = 'No sale in progress. Try scanning a product';
+var text = '';
 
 class Display {
 
-    setText(value) {
-        text = value
-        return text;
-    }
+  setText(value) {
+    text = value
+    return text;
+  }
 
-    getText() {
-        return text;
-    }
+  getText() {
+    return text;
+  }
 
-    displayPrice(priceAsText) {
-        return this.setText(priceAsText)
-    }
+  displayPrice(priceAsText) {
+    return this.setText(priceAsText)
+  }
 
-    displayEmptyBarcodeMessage(){
-        return this.setText('Scanning Error: Empty Barcode')
-    } 
-    
-    displayProductNotFoundMessage(barcode) {
-        return this.setText(`Product not found for ${barcode}`);
-    }
+  displayEmptyBarcodeMessage(){
+    return this.setText('Scanning Error: Empty Barcode')
+  } 
+  
+  displayProductNotFoundMessage(barcode) {
+    return this.setText(`Product not found for ${barcode}`);
+  }
+
+  displayNoSaleInProgressMessage() {
+    return this.setText('No sale in progress. Try scanning a product');
+  }
+  
 }
 
 module.exports = Display.prototype;
