a0ed70398cfc91b98cf690893704b1b9863b52e9
joypatel04 <joypatel041994@gmail.com>
2017-04-17 08:09:12 -0700

    Make test name more appropiate

--- __tests__/services/ComputePurchaseTotalTest.js
+++ __tests__/services/ComputePurchaseTotalTest.js
@@ -2,15 +2,15 @@ import Sale from './../../src/services/sale';
 
 describe('ComputePurchaseTotal Tests', () => {
 
-  it('Zero Items', () => {
+  it('Zero Items [] to Equal 0', () => {
       expect(Sale.computePurchaseTotal([])).toEqual(0);
   });
 
-  it('One Item', () => {
+  it('One Item [795] to Equal 795', () => {
       expect(Sale.computePurchaseTotal([795])).toEqual(795);
   });
 
-  it('Sevral Items', () => {
+  it('Sevral Items [850, 1275, 325] to Equal 2450', () => {
       expect(Sale.computePurchaseTotal([850, 1275, 325])).toEqual(2450);
   });
 
