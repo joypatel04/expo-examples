5b7c5ab563e9115b1f1d3c2f75f43c9631341401
joypatel04 <joypatel041994@gmail.com>
2017-04-16 19:29:27 -0700

    Rename to ScanOneItem

---
+++ __tests__/services/ScanOneItem-test.js
@@ -0,0 +1,20 @@
+import Sale from './../../src/services/sale';
+
+describe('ScanOneItem Tests', () => {
+    it('Product Found', () => {
+        expect(Sale.saleItem('12345')).toEqual('$7.95');
+    });
+
+    it('Another Product Found', () => {
+        expect(Sale.saleItem('23456')).toEqual('$12.50');
+    });
+
+    it('Product Not Found', () => {
+        expect(Sale.saleItem('99999')).toEqual('Product not found for 99999');
+    });
+
+    it('Scanning Error', () => {
+        expect(Sale.saleItem('')).toEqual('Scanning Error: Empty Barcode');
+    });
+});
+
