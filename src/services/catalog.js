3dc758acbb118ef7134b10f531ec027b9a9d9b73
joypatel04 <joypatel041994@gmail.com>
2017-04-16 19:29:43 -0700

    Fix Spaces to 2

--- src/services/catalog.js
+++ src/services/catalog.js
@@ -3,9 +3,9 @@ PricesByBarcode.set('12345', '$7.95');
 PricesByBarcode.set('23456', '$12.50');
 
 class Catalog {
-    findPrice(barcode) {
-        return PricesByBarcode.get(barcode)
-    }
+  findPrice(barcode) {
+    return PricesByBarcode.get(barcode)
+  }
 }
 
 module.exports = Catalog.prototype;
--- src/services/sale.js
+++ src/services/sale.js
@@ -5,24 +5,28 @@ import Display from './display';
    
 class Sale {
 
-    saleItem(barcode) {
-        return this.onBarcode(barcode);
-    }
+  saleItem(barcode) {
+    return this.onBarcode(barcode);
+  }
+  
+  onBarcode(barcode) {
+    var item = {};
     
-    onBarcode(barcode) {
-        var item = {};
-        
-        if ('' === barcode) {
-            return Display.displayEmptyBarcodeMessage();
-        }
-
-        var priceAsText = Catalog.findPrice(barcode)
-        if (priceAsText === null || priceAsText === undefined) {
-            return Display.displayProductNotFoundMessage(barcode);
-        } else {
-            return Display.displayPrice(priceAsText);
-        }
+    if ('' === barcode) {
+      return Display.displayEmptyBarcodeMessage();
     }
+
+    var priceAsText = Catalog.findPrice(barcode)
+    if (priceAsText === null || priceAsText === undefined) {
+      return Display.displayProductNotFoundMessage(barcode);
+    } else {
+      return Display.displayPrice(priceAsText);
+    }
+  }
+
+  onTotal() {
+    return Display.displayNoSaleInProgressMessage();
+  }
 
 }
 
